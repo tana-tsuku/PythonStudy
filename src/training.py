f485bbc6ad5b5f26de7bc07a1b420576cfcbd5ef
tanaka_tsukuhana <50260426+tana-tsuku@users.noreply.github.com>
2025-06-17 20:51:03 +0900

    update

--- src/training.py
+++ src/training.py
@@ -1,1 +1,23 @@
-print("Hello, this is the training module.")
+# 勉強用にソースコードのメモを書いたファイルです
+# 複数の機能があったとしても、機能に一貫性はありません。
+
+
+
+
+# 1．Outlookを操作するコード 新しいOutlookでは動かない
+""" import win32com.client
+
+try:
+    print("Outlook.Application を取得中...")
+    outlook = win32com.client.Dispatch("Outlook.Application")
+    print("成功: Outlook.Application オブジェクトを取得")
+
+    print("MAPI ネームスペースを取得中...")
+    namespace = outlook.GetNamespace("MAPI")
+    print("成功: MAPI オブジェクト取得")
+
+except Exception as e:
+    print("失敗:", repr(e))
+ """
+ 
+ 
