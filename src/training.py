327cb1dc24698e89a25eb7b0b0c43d70a8b67cd2
tanaka_tsukuhana <50260426+tana-tsuku@users.noreply.github.com>
2025-07-17 23:01:34 +0900

    7/17

--- src/training.py
+++ src/training.py
@@ -1,24 +1,12 @@
 # 勉強用にソースコードのメモを書いたファイルです
 # 複数の機能があったとしても、機能に一貫性はありません。
 
+from datetime import date
+
 # test.txtファイルｗを読み込む
 with open("test.txt", "r", encoding="utf-8") as file:
     content = file.readline()
 
-
-# 1．Outlookを操作するコード 新しいOutlookでは動かない
-""" import win32com.client
-
-try:
-    print("Outlook.Application を取得中...")
-    outlook = win32com.client.Dispatch("Outlook.Application")
-    print("成功: Outlook.Application オブジェクトを取得")
-
-    print("MAPI ネームスペースを取得中...")
-    namespace = outlook.GetNamespace("MAPI")
-    print("成功: MAPI オブジェクト取得")
-
-except Exception as e:
-    print("失敗:", repr(e))
- """
-
+t = date.today()
+# 今日の日付を表示
+print(f"今日の日付は {t.year}年{t.month}月{t.day}日です。")
